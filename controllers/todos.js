01d004bce0da9e5516408fd370a433234bd18975
arzansa <arzansa@gmail.com>
2024-08-26 15:25:16 -0500

    add inventory button and route

--- controllers/todos.js
+++ controllers/todos.js
@@ -4,6 +4,11 @@ const ensureLoggedIn = require('../middleware/ensureLoggedIn');
 
 // Fake "protected" route that would require
 // a logged in user.
+
+router.get("/inventory", (req, res) => {
+  res.render("inventory/index.ejs");
+});
+
 // GET /todos/new
 router.get('/new', ensureLoggedIn, (req, res) => {
   res.send('Yay, you were logged in!');
